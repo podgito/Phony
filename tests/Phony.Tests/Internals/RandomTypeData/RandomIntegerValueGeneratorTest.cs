7a3d3617cc8e1b0f441b270e22e882aafe5f5a8a
Padraic Duffy <Padraic Duffy>
2016-07-13 22:04:10 +0100

    Phony Internals visible to the test project RandomIntegerValue Unit tests

---
+++ tests/Phony.Tests/Internals/RandomTypeData/RandomIntegerValueGeneratorTest.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using Phony.Internals.RandomTypeData;
+using Shouldly;
+
+namespace Phony.Tests.Internals.RandomTypeData
+{
+    [TestFixture]
+    public class RandomIntegerValueGeneratorTest
+    {
+        private RandomIntegerValueGenerator intGenerator;
+
+        [SetUp]
+        public void Setup()
+        {
+            intGenerator = new RandomIntegerValueGenerator(0, 10);
+        }
+
+        [Test]
+        public void All_Values_Within_Range()
+        {
+            //Act
+            for (int i = 0; i < 1000; i++)
+            {
+                var value = (int)intGenerator.GenerateValue();
+                value.ShouldBeInRange(0, 10);
+            }
+        }
+    }
+}
