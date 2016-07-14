f1ba0f9e600f684bb2a5e0938023302685d42093
Padraic Duffy <Padraic Duffy>
2016-07-14 23:25:59 +0100

    Added Random selector generator and tests

---
+++ src/Phony/Internals/RandomTypeData/RandomValueSelectorGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phony.Internals.RandomTypeData
+{
+    internal class RandomValueSelectorGenerator<T> : RandomTypeValueGeneratorBase<T>
+    {
+        private readonly T[] selection;
+
+        public RandomValueSelectorGenerator(params T[] selection)
+        {
+            this.selection = selection;
+        }
+
+        public override object GenerateValue()
+        {
+            var rand = new Random();
+            var randomIndex = rand.Next(0, selection.Count());
+            return selection[randomIndex];
+        }
+    }
+}
---
+++ tests/Phony.Tests/Internals/RandomTypeData/RandomValueSelectorGeneratorTest.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using Phony.Internals.RandomTypeData;
+using Shouldly;
+
+namespace Phony.Tests.Internals.RandomTypeData
+{
+    [TestFixture]
+    public class RandomValueSelectorGeneratorTest
+    {
+        [Test]
+        public void SelectsValueFromList()
+        {
+            var values = new string[] { "123", "abc", "a1b2", "test" };
+            var generator = new RandomValueSelectorGenerator<string>(values);
+            for (int i = 0; i < 100; i++)
+            {
+                var value = (string)generator.GenerateValue();
+                value.ShouldBeOneOf(values);
+            }
+        }
+
+        [Test]
+        public void WithOnlyOneValueItShouldAlwaysReturnIt()
+        {
+            var values = new string[] { "Phony" };
+            var generator = new RandomValueSelectorGenerator<string>(values);
+            for (int i = 0; i < 100; i++)
+            {
+                var value = (string)generator.GenerateValue();
+                value.ShouldBeOneOf(values);
+            }
+        }
+    }
+}
